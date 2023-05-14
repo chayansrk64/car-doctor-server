9f4c320b7abc8588b051fd3f43494a2f37cbe37d
chayansrk64 <chayansrk64@gmail.com>
2023-05-14 07:53:39 +0600

    jwt token

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -23,6 +24,51 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// const verifyJWT = (req, res, next) => {
+//   console.log("Hitting the JWT");
+//   console.log(req.headers.authorization);
+//   const authorization = req.headers.authorization;
+//   if(!authorization){
+//     return  res.status(401).send({error:true, message: "Unauthorized access"})
+//   }
+
+//   const token = authorization.split(' ')[1]
+//   console.log("token inside JWT", token);
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded)=> {
+//      if(error){
+//         return res.status(403).send({error:true, message:"Unauthorised access"})
+//      }
+//      req.decoded = decoded;
+//      next()
+//   })
+// }
+
+
+const verifyJWT = (req, res, next) => {
+    console.log("HITTING THE JWT");
+    // step: 1
+    const authorization = req.headers.authorization;
+    if(!authorization){
+       return res.status(401).send({error:true, message:"Unauthorized access"})
+    }
+    // step: 2
+    const token = authorization.split(' ')[1];
+    console.log('token inside JWT', token);
+    // step: 3
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if(error){
+            return  res.status(403).send({error:true, message: "Unauthorized access"})
+        }
+        req.decoded = decoded;
+        next()
+    })
+    
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +77,16 @@ async function run() {
     const serviceCollection = client.db('carDoctor').collection('services');
     const bookingCollection = client.db('carDoctor').collection('bookings');
 
+    // JWT 
+    app.post('/jwt', (req, res)=> {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token})
+    })
+
+
+    // SERVICE ROUTE
     app.get('/services', async(req, res)=> {
         const cursor = serviceCollection.find()
         const result = await cursor.toArray()
@@ -51,8 +107,11 @@ async function run() {
     })
 
     //send bookings to ui (by query search) (read)
-    app.get('/bookings', async(req, res) => {
-        console.log(req.query.email);
+    app.get('/bookings', verifyJWT, async(req, res) => {
+        const decoded = req.decoded;
+        
+        // console.log(req.headers.authorization);
+        console.log('After veryfity jwt token', decoded);
         let query = {};
         if(req.query?.email){
             query = {email: req.query.email}
